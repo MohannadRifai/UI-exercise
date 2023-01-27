1b31a94f22c1c1dd17cc8d974643a695db7d6e8a
Mohannad <mohannadalrifai7@gmail.com>
2023-01-28 00:42:54 +0200

    first

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import './header.css';
+
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">Welcome to Rifai free GPT OpenAI</h1>
+      <p>This is a pre-trained language model developed by our company. It is based on the GPT architecture and is fine-tuned for conversational response generation.</p>
+
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} />
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+    </div>
+
+    <div className="gpt3__header-image">
+      <img src={ai} />
+      
+    </div>
+  </div>
+);
+
+export default Header;
